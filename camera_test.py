5b2a657aad75bbff8195be45d37bce9742477fb1
Olena <57922241+oli-e@users.noreply.github.com>
2021-06-03 00:38:13 +0200

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -4,12 +4,10 @@ video = cv2.VideoCapture(0)
 
 while True:
 	ret, frame = video.read()
-	cv2.imshow('original', frame)
-	cv2.imwrite('original.jpg', frame)
+	cv2.imshow('test', frame)
 
 	key = cv2.waitKey(1)
 	if key == 27:
 		break
-
 video.release()
 cv2.destroyAllWindows()
